bb63eb4ec7bc1a804bf59c16fcc15b62c7625537
r06922132 <43261914+r06922132@users.noreply.github.com>
2018-11-29 23:44:22 +0800

    Add files via upload

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -O best_model.h5 'https://www.dropbox.com/s/oqbpqmreeyt3i2p/best_model.h5?dl=0'
+wget -O best_model.h5 'https://www.dropbox.com/s/oqbpqmreeyt3i2p/best_model.h5?dl=1'
 
 python3 hw3_test.py $1 $2
